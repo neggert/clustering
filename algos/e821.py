ed392d5f8da38a839739c05fbb05017dbd6a79dc
Nic Eggert <nic.eggert@gmail.com>
2013-07-30 14:07:02 -0400

    Add mock-up of algo used in E821

---
+++ algos/e821.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def fit_waveform(wf, thresh_frac=0.3, count_limit=5):
+    """
+    Fit a waveform. Decides whether there are 1 or two signals present
+    by counting the number of time samples above a threshold. Sums over
+    spatial information.
+
+    Params:
+    wf - (5*7*?) numpy array giving the digitized waveform
+    thresh_frac - fraction of the maximum to use as the threshold
+
+    Returns:
+    ((x0, y0, t0), ...) List of tuples. One tuple for each cluster the algorithm find.
+                        Each tuple gives the centroid of the cluster in x, y, and time.
+    """
+
+    # ignore all spatial information
+    ts = wf.sum(axis=(0, 1))
+
+    # find a threhold
+    threshold = thresh_frac*ts.max()
+
+    # find the number of samples above the threshold
+    counts = np.count_nonzero(ts > threshold)
+
+    if counts > count_limit:
+        return ((None, None, None), (None, None, None))
+    else:
+        return ((None, None, None), )
