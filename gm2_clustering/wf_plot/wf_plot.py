eae40491b8d74ff93afc42673e940baca55c4dad
Nic Eggert <nic.eggert@gmail.com>
2013-08-30 16:04:46 -0400

    Add function to plot waveforms

--- gm2_clustering/wf_plot/wf_plot.py
+++ gm2_clustering/wf_plot/wf_plot.py
@@ -6,10 +6,10 @@ def plot_waveform(data):
     plt.figure(figsize=(24, 16))
     up = data.max()
     gs = gridspec.GridSpec(6, 9)
-    for i in xrange(6):
-        for j in xrange(9):
+    for i in xrange(9):
+        for j in xrange(6):
             x = data[i, j, :]
-            ax = plt.subplot(gs[i, j])
+            ax = plt.subplot(gs[j, i])
             ax.plot(x)
             ax.set_ylim(0.1, up)
             ax.set_yscale("log", nonposy="clip")
