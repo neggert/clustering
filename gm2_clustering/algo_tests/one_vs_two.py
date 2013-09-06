bed2625c44d63180c027cbcfbe922eb9f715dacf
Nic Eggert <nic.eggert@gmail.com>
2013-09-06 10:42:48 -0400

    parallelize testing using multiprocessing

--- gm2_clustering/algo_tests/one_vs_two.py
+++ gm2_clustering/algo_tests/one_vs_two.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from gm2_clustering.wf_generator import *
 import math
 import numpy as np
-
+import multiprocessing
 
 def one_vs_two(fit_func, data_file):
     """
@@ -32,17 +32,68 @@ def one_vs_two(fit_func, data_file):
     incorrect_dr, total_dr = [], []
     incorrect_dt, total_dt = [], []
 
+    def num_clusters(wf):
+        return len(fit_func(wf))
+
+    def worker(in_q, out_q):
+        while True:
+            i = in_q.get()
+            if i is None:  # poison pill
+                print "Quitting worker"
+                in_q.task_done()
+                return
+            p1, p2, wf = i
+            n = num_clusters(wf)
+            out_q.put((p1, p2, n))
+            in_q.task_done()
+
     with np.load(data_file) as data:
+        job_queue = multiprocessing.JoinableQueue()
+        out_queue = multiprocessing.Queue()
+
+        workers = []
+        for i in xrange(8):
+            w = multiprocessing.Process(target=worker, args=(job_queue, out_queue))
+            w.start()
+            workers.append(w)
+
+        # Send data to workers
+        n_one = 0
         for line in data['one']:
-            params, wf = line
-            n = len(fit_func(wf))
+            p1, wf = line
+            job_queue.put((p1, None, wf))
+            n_one += 1
+
+        # wait for workers to finish
+        print "Waiting for workers to finish"
+        job_queue.join()
+
+        # retrieve the data
+        print "Getting data"
+        for i in xrange(n_one):
+            p1, p2, n = out_queue.get()
             if n == 1:
                 n_one_correct += 1
             n_one_tries += 1
 
+        assert(out_queue.empty())
+
+        n_two = 0
         for line in data['two']:
             p1, p2, wf = line
-            n = len(fit_func(wf))
+            job_queue.put((p1, p2, wf))
+            n_two += 1
+
+        print "Waiting for workers to finish"
+        job_queue.join()
+
+
+        print "Getting data"
+        for i in xrange(n_two):
+            p1, p2, n = out_queue.get()
+            if p2 is None:
+                import pdb
+                pdb.set_trace()
             dr = math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
             dt = abs(p1[2]-p2[2])
             if n < 2:
@@ -51,9 +102,14 @@ def one_vs_two(fit_func, data_file):
             total_dr.append(dr)
             total_dt.append(dt)
 
+        # shut down the workers
+        for i in xrange(8):
+            job_queue.put(None)
+
+
     print("False-positive rate: {}".format(1-1.*n_one_correct/n_one_tries))
 
-    total_count, bin_x, bin_y = np.histogram2d(total_dr, total_dt, bins=(4,20))
+    total_count, bin_x, bin_y = np.histogram2d(total_dr, total_dt, bins=(8, 25))
     incorrect_count, bin_x, bin_y = np.histogram2d(incorrect_dr, incorrect_dt, bins=[bin_x, bin_y])
     ratio = 1.*incorrect_count/total_count
     ratio[np.isnan(ratio)] = 0.
@@ -64,4 +120,3 @@ def one_vs_two(fit_func, data_file):
     cb = plt.colorbar()
     cb.set_label("False-negative rate")
     plt.show()
-    raw_input()
